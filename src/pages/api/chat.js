11542de25819809f4ced1037e0d7173295cf9b15
Pku <pku@Pkus-MacBook-Pro.local>
2023-10-04 23:16:33 +0530

    initial message added

--- src/pages/api/chat.js
+++ src/pages/api/chat.js
@@ -2,42 +2,33 @@
 import axios from 'axios'
 
 export default async function handler (req, res) {
-  
-  console.log('reached api call handler');
-  
+  console.log('reached api call handler')
+
   const referer = req.headers.referer || req.headers.referrer // get the referer from the request headers
 
   if (req.method !== 'POST') {
-          console.log('1');
     res.status(405).json({ message: 'Method should be POST' })
-  // } else if (process.env.NODE_ENV !== 'development') {
-  //   console.log('2');
-  //   if (!referer || referer !== process.env.APP_URL) {
-  //     console.log('3');
-  //     res.status(401).json({ message: 'Unauthorized' })
-  //   }
+    // } else if (process.env.NODE_ENV !== 'development') {
+    //   console.log('2');
+    //   if (!referer || referer !== process.env.APP_URL) {
+    //     console.log('3');
+    //     res.status(401).json({ message: 'Unauthorized' })
+    //   }
   } else {
-    console.log('4');
     try {
-      console.log('5');
       const { body } = req
       const url = 'https://api.openai.com/v1/chat/completions'
       const headers = {
         'Content-type': 'application/json',
         Authorization: `Bearer ${process.env.OPENAI_API_KEY}`
       }
-      console.log('6');
-      console.log('hitting api');
+
       const response = await axios.post(url, body, { headers: headers })
-console.log('7');
+
       res.status(200).json(response.data)
-      console.log('8');
-    } catch (error)  {
-console.log('9');
+    } catch (error) {
       console.log(error)
       res.status(500).json({ message: 'Something went wrong' })
     }
-    console.log('10');
   }
-  console.log('11');
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,16 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home () {
   const [inputValue, setInputValue] = useState('')
-  const [chatLog, setChatLog] = useState([])
+  const [chatLog, setChatLog] = useState([
+    // {
+    //   type: 'user',
+    //   message: 'hi'
+    // },
+    // {
+    //   type: 'assistant',
+    //   message: 'hello'
+    // }
+  ])
   const [isLoading, setIsLoading] = useState(false)
 
   const handleSubmit = event => {
@@ -74,39 +83,69 @@ export default function Home () {
   }
 
   return (
-    <div className='container mx-auto max-w-[700px]'>
-      <div className='flex flex-col h-screen bg-gray-900'>
+    <div className='container mx-auto max-w-[700px] bg-gray-900'>
+      <div
+        className='flex flex-col h-screen bg-gray-900'
+        // style={{ height: !!chatLog.length ? '100%' : '90%' }}
+      >
         <h1 className='bg-gradient-to-r from-blue-500 to-purple-500 text-transparent bg-clip-text text-center py-3 font-bold text-6xl'>
           EchoDost ChatBot
         </h1>
-        <div className='flex-grow p-6 mainDiv' style={{ overflowY: 'scroll' }}>
-          <div className='flex flex-col space-y-4'>
-            {chatLog.map((message, index) => (
-              <div
-                key={index}
-                className={`flex ${
-                  message.type === 'user' ? 'justify-end' : 'justify-start'
-                }`}
+
+        {!chatLog.length ? (
+          <div
+            className='text-center text-white p-6'
+            // style={{ marginTop: '15%', marginBottom: '20%' }}
+          >
+            <p>Say Hi! to start chatting with the bot.</p>
+            <p className='p-6'>Made by Ram for WWF-India.</p>
+            <p>
+              Source code at{' '}
+              <a
+                href='https://github.com/gurumukhi/EchoDostChatBot/'
+                target='_blank'
               >
-                <div
-                  className={`${
-                    message.type === 'user' ? 'bg-purple-500' : 'bg-gray-800'
-                  } rounded-lg p-4 text-white max-w-sm`}
-                >
-                  {message.message}
-                </div>
-              </div>
-            ))}
-            {isLoading && (
-              <div key={chatLog.length} className='flex justify-start'>
-                <div className='bg-gray-800 rounded-lg p-4 text-white max-w-sm'>
-                  <TypingAnimation />
-                </div>
-              </div>
-            )}
+                https://github.com/gurumukhi/EchoDostChatBot/
+              </a>
+            </p>
           </div>
-        </div>
-        <form onSubmit={handleSubmit} className='flex-none p-6'>
+        ) : (
+          <div
+            className='flex-grow p-6 mainDiv'
+            style={{ overflowY: 'scroll' }}
+          >
+            <div className='flex flex-col space-y-4'>
+              {chatLog.map((message, index) => (
+                <div
+                  key={index}
+                  className={`flex ${
+                    message.type === 'user' ? 'justify-end' : 'justify-start'
+                  }`}
+                >
+                  <div
+                    className={`${
+                      message.type === 'user' ? 'bg-purple-500' : 'bg-gray-800'
+                    } rounded-lg p-4 text-white max-w-sm`}
+                  >
+                    {message.message}
+                  </div>
+                </div>
+              ))}
+              {isLoading && (
+                <div key={chatLog.length} className='flex justify-start'>
+                  <div className='bg-gray-800 rounded-lg p-4 text-white max-w-sm'>
+                    <TypingAnimation />
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        )}
+        <form
+          onSubmit={handleSubmit}
+          className='flex-none p-6'
+          style={{ marginBottom: !chatLog.length ? '10%' : '0px' }}
+        >
           <div className='flex rounded-lg border border-gray-700 bg-gray-800'>
             <input
               type='text'
